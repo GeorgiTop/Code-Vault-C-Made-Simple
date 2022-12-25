be57b002136c81de82c0f30c889282b1d69663c1
GeorgiTop <88086563+GeorgiTop@users.noreply.github.com>
2022-12-25 21:29:00 +0200

    Added 049;

---
+++ 049 How to parse a string in C (sscanf)/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char const *argv[] )
+{
+    char str[] = "Andrew;New York;20;0";
+
+
+    char         name[20] = { 0 };
+    char         city[20] = { 0 };
+    unsigned int age;
+    int          hasDegree;
+
+    sscanf(str, "%[^;];%[^;];%u;%d", name, city, &age, &hasDegree);
+    printf("%s is in %s and is %u years old\n", name, city, age, hasDegree);
+
+    return 0;
+}
