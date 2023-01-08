3293765ceb632bfdef0e29ddc97fe39de61ca72b
GeorgiTop <88086563+GeorgiTop@users.noreply.github.com>
2023-01-08 20:52:18 +0200

    Added 084;

---
+++ 084 Iterating over doubly linked lists/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node
+{
+    int          x;
+    struct Node *next;
+    struct Node *prev;
+};
+typedef struct Node Node;
+
+int main( int argc, char const *argv[] )
+{
+    Node *tail = malloc( sizeof( Node ) );
+    if ( tail == NULL )
+    {
+        exit( 1 );
+    }
+    tail->x    = 1;
+    tail->prev = NULL;
+    tail->next = malloc( sizeof( Node ) );
+    if ( tail->next == NULL )
+    {
+        exit( 1 );
+    }
+    tail->next->x    = 2;
+    tail->next->prev = tail;
+    tail->next->next = malloc( sizeof( Node ) );
+    if ( tail->next->next == NULL )
+    {
+        exit( 1 );
+    }
+    tail->next->next->x    = 7;
+    tail->next->next->prev = tail->next;
+    tail->next->next->next = NULL;
+    Node *head = tail->next->next;
+
+    Node *curr;
+    for ( curr = tail; curr != NULL; curr = curr->next )
+    {
+        printf( "%d\n", curr->x );
+    }
+
+    curr = head;
+    while(curr != NULL)
+    {
+        printf( "%d\n", curr->x );
+        curr = curr->prev;
+    }
+    return 0;
+}
